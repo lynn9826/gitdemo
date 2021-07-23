8bfb1d5551620aa5955103290e7b77494a15959e
lynn9826 <179783049@qq.com>
2021-07-23 10:56:45 +0800

    well well

---
+++ untitled/src/com/lanqiao/well/Test.java
@@ -0,0 +1,13 @@
+package com.lanqiao.well;
+
+/**
+ * @Description
+ * @Author Lynn
+ * @Date 2021/7/23 0023 ;10:56
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("well well");
+    }
+}
